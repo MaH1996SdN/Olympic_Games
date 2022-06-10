aa4b04731725acae1980ce8bcacf68b139fa38bb
Mahsa <mahsa.sedaghatinia@studenti.univr.it>
2022-06-10 10:09:56 +0200

    adding more exploration on medals, adding charts

--- programming_project_olympic.py
+++ programming_project_olympic.py
@@ -73,9 +73,12 @@ olympic_df = olympic_df[olympic_df['Country'].notna()]
 olympic_df.drop(olympic_df.loc[olympic_df['Sport']=='Art Competitions'].index, inplace=True)
 
 Medals = olympic_df.loc[olympic_df['Medal'] != 0 ]
+goldMedals = olympic_df.loc[olympic_df['Medal'] == 1]
 
 femaleParticipants = olympic_df.loc[(olympic_df['Sex'] == 'F')]
 femaleMedalists = femaleParticipants.loc[femaleParticipants['Medal'] != 0]
+femaleGoldMedalists = femaleParticipants.loc[femaleParticipants['Medal'] == 1]
+
 
 
 
@@ -89,10 +92,28 @@ st.text("")
 st.text("")
 
 
+# #adding sidebar
+# with st.sidebar:
+#     st.write("check out Github [link](https://github.com/MaH1996SdN/Programming_Project)")
+
+
 #**************************EXPLORATION********************************************************************************************************************
 st.subheader('Exploration')
 st.text("")
+#medals
+with st.expander('Medals'):
+   
+    col_21, col_22, col_23 = st.columns(3)
+    with col_21:
+        st.metric('number of Gold medals:', olympic_df['Medal'].value_counts()[1])
+    with col_22:
+        st.metric('number of Silver medals:', olympic_df['Medal'].value_counts()[2])
+    with col_23:
+        st.metric('number of Bronze medals:', olympic_df['Medal'].value_counts()[3])
+    st.metric('Total number of medals:', Medals.shape[0])
 
+
+#Countries
 with st.expander('Exploration on countries'):
     col_1, col_2 = st.columns(2)
     with col_1:
@@ -118,29 +139,128 @@ with st.expander('Exploration on countries'):
         st.dataframe(Medals.Country.value_counts().tail(5))
         st.text("")
 
-with st.expander('Exploration on sports'):
     col_5, col_6 = st.columns(2)
     with col_5:
+        st.write('Number of Gold medals by country/ Top 5')
+        # top 5 countries by number of gold medals
+        st.dataframe(goldMedals.Country.value_counts().head(5))
+        st.text("")
+    with col_6:
+        st.write('Number of Gold medals by country/ Last 5')
+        # last 5 countries by number of gold medals
+        st.dataframe(goldMedals.Country.value_counts().tail(5))
+        st.text("")
+
+
+
+#Sports
+with st.expander('Exploration on sports'):
+    col_9, col_10 = st.columns(2)
+    with col_9:
         st.write('Top 5 sports by number of medals')
         # top 5 sports by number of medals
         st.dataframe(Medals.Sport.value_counts().head(5))
         st.text("")
-    with col_6:
+    with col_10:
         st.write('Last 5 sports by number of medals')
         # last 5 sports by number of medals
         st.dataframe(Medals.Sport.value_counts().tail(5))
         st.text("")
 
+
+
+
+#women in olympics
 with st.expander('Exploration on women in Olympics'):
-    col_3, col_4 = st.columns(2)
-    with col_3:
-        st.write('female participants by country')
-        # number of female participants by country
+    col_13, col_14 = st.columns(2)
+    with col_13:
+        st.write('Female participants by country/ Top 5')
+        # top 5 countries by number of female participants
         st.dataframe(femaleParticipants.Country.value_counts().head(5))
         st.text("")
-    with col_4:
-        st.write('Top 5 countries with female medalists')
-        # number of female participants by country
-        femaleMedalists = femaleParticipants.loc[femaleParticipants['Medal'] != 0]
+    with col_14:
+        st.write('Female participants by country/ Last 5')
+        # last 5 countries by number of female participants
+        st.dataframe(femaleParticipants.Country.value_counts().tail(5))
+        st.text("")
+
+    col_15, col_16 = st.columns(2)
+    with col_15:
+        st.write('Number of medals by women in countries/ Top 5')
+        # top 5 countries by number of medals by women
         st.dataframe(femaleMedalists.Country.value_counts().head(5))
+        st.text("")
+    with col_16:
+        st.write('Number of medals by women in countries/ Last 5')
+        # last 5 countries by number of medals by women
+        st.dataframe(femaleMedalists.Country.value_counts().tail(5))
+        st.text("")
+
+    col_17, col_18 = st.columns(2)
+    with col_17:
+        st.write('Number of Gold medals by women in countries/ Top 5')
+        # top 5 countries by number of gold medals by women
+        st.dataframe(femaleGoldMedalists.Country.value_counts().head(5))
+        st.text("")
+    with col_18:
+        st.write('Number of Gold medals by women in countries/ Last 5')
+        # last 5 countries by number of gold medals by women
+        st.dataframe(femaleGoldMedalists.Country.value_counts().tail(5))
+        st.text("")
+
+#Correlation
+with st.expander('Correlation between features'):
+    st.dataframe(olympic_df.corr())
+
+#distrubution
+with st.expander('Distrubutions'):
+    col_19, col_20 = st.columns(2)
+    with col_19:
+        ax = plt.figure(figsize=(12,10))
+        olympic_df.Age.hist()
+        plt.title('Distrubution', fontweight='bold', fontsize=14)
+        st.write(ax)
+        st.caption('Distrubution of Participant\'s Age')
+    with col_20:
+        ax = plt.figure(figsize=(12,10))
+        olympic_df.Height.hist()
+        plt.title('Distrubution', fontweight='bold', fontsize=14)
+        st.write(ax)
+        st.caption('Distrubution of Participant\'s Height')
+
+
+#**************************Charts********************************************************************************************************************
+
+st.subheader('Charts')
+st.text("")
+
+# correlation graph
+with st.expander('Heatmap'):
+    ax = plt.figure(figsize=(12,10))
+    sns.heatmap(olympic_df.corr(), annot=True)
+    plt.title('Correlation', fontweight='bold', fontsize=14)
+    st.write(ax)
+
+
+
+# participants by sex
+with st.expander('Percentage of participants'):
+    ax = plt.figure(figsize=(10,6))
+    labels = ['Male', 'Female']
+    plt.pie(olympic_df['Sex'].value_counts(), labels=labels, autopct='%.2f%%', startangle=90, shadow=True)
+    plt.title('participants by sex', fontweight='bold', fontsize=14)
+    plt.legend()
+    st.write(ax)
+
+# top 5 medalists
+with st.expander('Top 5 medalists'):
+    ax = plt.figure(figsize=(10,6))
+    lb = Medals['Name'].value_counts().head(5)
+    labels = lb.index
+    plt.pie(Medals['Name'].value_counts().head(5),labels= labels, autopct='%.2f%%', startangle=90, shadow=True)
+    plt.title('Top 5 medalists', fontweight='bold', fontsize=14)
+    plt.legend()
+    plt.legend(loc="upper left")
+    st.write(ax)
+
 
